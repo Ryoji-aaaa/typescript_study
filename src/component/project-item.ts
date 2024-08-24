2e4feca0b430356e5a828de9c338ecead33a6b46
Ryoji-aaaa <r.koyama.047@icloud.com>
2024-08-24 15:47:50 +0900

    S11_Webpackの導入

--- src/component/project-item.ts
+++ src/component/project-item.ts
@@ -1,7 +1,7 @@
-import Component from "../component/base-component.js";
-import { Project } from "../models/project.js";
-import { Draggable } from "../models/drag-drop.js";
-import { autobind } from "../decorators/autobind.js";
+import Component from "../component/base-component";
+import { Project } from "../models/project";
+import { Draggable } from "../models/drag-drop";
+import { autobind } from "../decorators/autobind";
 
 //Project Item Class
 export class ProjectItem
