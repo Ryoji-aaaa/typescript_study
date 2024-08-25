aae88b8b928e7048b17ed1cbe0efd9daa5bbc3ec
Ryoji-aaaa <r.koyama.047@icloud.com>
2024-08-25 11:42:01 +0900

    S12_end

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,28 @@
-import { ProjectInput } from "./component/project-input";
-import { ProjectList } from "./component/project-list";
+// import _ from "lodash";
+import { Product } from "./product.model";
+import { plainToInstance } from "class-transformer";
+import { validate } from "class-validator";
 
-new ProjectInput();
-new ProjectList("active");
-new ProjectList("finished");
+const products = [
+  { title: "A Carpet", price: 2.99 },
+  { title: "A Book", price: 10.99 },
+];
 
+const loadedProduct = plainToInstance(Product, products);
+const newProd = new Product("", -5);
+validate(newProd).then(errors=>{
+    if(errors.length>0){
+        console.log("validation Errors!")
+        console.log(errors)
+    }else{
+        console.log(newProd.getInfomation());
+    }
+});
 
+for (const prod of loadedProduct) {
+  console.log(prod.getInfomation());
+}
+// const p1 = new Product("apple", 2);
+// const loadedProduct = products.map((prod) => {
+//   return new Product(prod.title, prod.price);
+// });
